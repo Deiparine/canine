514ae8c16727ac148620add5e8111b27bcfff9f7
Ian Josef <ian.rivera@webriq.services>
2020-05-19 17:53:25 +0800

    fix favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby + Sanity`,
+    title: `Colorado Canine Orthopedics`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
     siteUrl: `https://webriq-gatsby-cms-starter-template-sanity.webriq.me/`,
@@ -100,7 +100,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
