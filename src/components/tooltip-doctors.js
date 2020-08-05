cf8bb021d064496d8832aa55d6803149a4371183
zelth2020 <61266341+zelth2020@users.noreply.github.com>
2020-08-05 22:14:41 +0800

    Updated Doctors menu dropdown text transformation

--- src/components/tooltip-doctors.js
+++ src/components/tooltip-doctors.js
@@ -9,20 +9,20 @@ const TooltipDoctors = () => {
         <ul>
           <li>
             <Link to="/doctors/bauer-dvm" style={{ textTransform: "none" }}>Dr. Michael Bauer DVM DACVS</Link>
-            <Link to="/doctors/dr-michael-detora">
+            <Link to="/doctors/dr-michael-detora" style={{ textTransform: "none" }}>
               ​​Dr. Michael Detora DVM DACVS
             </Link>
-            <Link to="/doctors/dr-todd-riecks">
+            <Link to="/doctors/dr-todd-riecks" style={{ textTransform: "none" }}>
               ​​Dr. Todd Riecks DVM DACVS
             </Link>
-            <Link to="/doctors/dr-scot-swainson">
+            <Link to="/doctors/dr-scot-swainson" style={{ textTransform: "none" }}>
               ​​Dr. Scot Swainson DVM DACVS
             </Link>
-            <Link to="/doctors/dr-nate-miller">Dr. Nate Miller DVM DACVS</Link>
-            <Link to="/doctors/dr-sam-franklin">
+            <Link to="/doctors/dr-nate-miller" style={{ textTransform: "none" }}>Dr. Nate Miller DVM DACVS</Link>
+            <Link to="/doctors/dr-sam-franklin" style={{ textTransform: "none" }}>
               ​​Dr. Sam Franklin DVM PhD DACVS DACVSMR
             </Link>
-            <Link to="/doctors/dr-andra-detora">
+            <Link to="/doctors/dr-andra-detora" style={{ textTransform: "none" }}>
               ​​Dr. Andra DeTora DVM CCRT cVMA
             </Link>
           </li>
